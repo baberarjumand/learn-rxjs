780384cf0503eb591de2d94a20962633e4199f0f
baberarjumand <baber.arjumand@gmail.com>
2020-03-15 11:48:31 +0300

    initial commit prettified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,7 +63,9 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const path = window.location.pathname.split('folder/')[1];
     if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+      this.selectedIndex = this.appPages.findIndex(
+        page => page.title.toLowerCase() === path.toLowerCase()
+      );
     }
   }
 }
