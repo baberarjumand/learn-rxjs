2c53c0ff710d7df53640cd246bf2e9690a265c0d
baberarjumand <baber.arjumand@gmail.com>
2020-03-17 11:10:23 +0300

    putting project on hold for now

--- src/app/prereqs/prereqs.page.ts
+++ src/app/prereqs/prereqs.page.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-prereqs',
   templateUrl: './prereqs.page.html',
-  styleUrls: ['./prereqs.page.scss'],
+  styleUrls: ['./prereqs.page.scss']
 })
 export class PrereqsPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
